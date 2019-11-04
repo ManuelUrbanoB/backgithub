ee8f943472937fca4e2edc1da77c5af7c60e721a
Manuel Urbano <manuelurbanobenavides@gmail.com>
2019-11-04 11:32:44 -0500

    add end point login method POST with username and password

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var request = require('request');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
 });
+
+
+router.post('/login', function(req, res, next) {
+    console.log(req.body);
+    var username  = req.body.username;
+    var password  = req.body.password;
+    var options = {
+    url: 'https://api.github.com',
+    auth: {
+        'user': username,
+        'pass': password
+    },
+    headers: {
+      'User-Agent':'' 
+    }
+  };
+
+  function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+          res.status(200).send({success: true, body: JSON.parse(body)})
+          return
+      }else {
+        res.status(response.statusCode).send({success: false, body: JSON.parse(body)})
+      }
+  }
+  request(options, callback);
+});
+
+
 
 module.exports = router;
